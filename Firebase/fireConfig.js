0cb27ae7be0b07cceea81d669368ed0ddd35b9ca
RakelSousa17 <rakelsolsasilva@gmail.com>
2019-11-18 21:32:08 -0300

    Firebase

---
+++ Firebase/fireConfig.js
@@ -0,0 +1,8 @@
+var fbConfig = {
+    apiKey: "AIzaSyAnD5vtoX3N38SNP5iHfVJQd0nxsULw94c",
+    authDomain: "aulas-de-cordova.firebaseapp.com",
+    databaseURL: "https://aulas-de-cordova.firebaseio.com",
+    projectId: "aulas-de-cordova",
+    storageBucket: "aulas-de-cordova.appspot.com",
+    messagingSenderId: "25796691453"
+};
